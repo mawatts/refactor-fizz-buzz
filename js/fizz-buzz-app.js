fe9c6593e7e7719cc72d4c7545c15b52955be758
mawatts <michaelwatts80@yahoo.com>
2015-04-12 11:25:23 -0400

    Add completed refactored fizz buzz app

--- js/fizz-buzz-app.js
+++ js/fizz-buzz-app.js
@@ -1,39 +1,72 @@
 $(document).ready(function() {
-	startFizzBuzz();
-	resetFizzBuzz();
+	numberPrompt();
+	reloadFizzBuzz();
 });
 
-function startFizzBuzz() {
-	$('.startButton').click(function() {
-		var x = 0;
-		for (var i = 0; i <= 100; i++) {
-			if (i % 3 == 0 && i % 5 == 0) {
-				x = "Fizz Buzz";
-			}
-			else if (i % 3 == 0) {
-				x = "Fizz";
-			}
-			else if (i % 5 == 0) {
-				x = "Buzz";
-			}
-			else {
-				x = i;
-			}
+var inputValue = "";
 
-			if (i == 100) {
-				document.getElementById("results").innerHTML = document.getElementById("results").innerHTML + x + ".";
-			}
-			else {
-				document.getElementById("results").innerHTML = document.getElementById("results").innerHTML + x + ", ";
-			}
+function numberPrompt() {
+	swal({
+	   title: "Number Input",
+	   text: "Enter a number for use in the Fizz Buzz App:",
+	   type: "input",
+	   showCancelButton: true,
+	   closeOnConfirm: false,
+	   animation: true,
+	   confirmButtonColor: "#FF9100"
+	},
+	function(inputTxt) {
+		inputValue = +document.querySelector('input').value;
+		if (isNaN(inputTxt) == true) {
+			swal.showInputError("You need to enter a number not a string!");
+		}
+		else if (inputTxt % 1 != 0) {
+			swal.showInputError("You need to enter a whole number not a decimal!");
+		}
+		else {
+		   	if (inputTxt === false) return false;
+
+		    if (inputTxt === "") {
+		    	document.getElementById("results").innerHTML = " ";
+		    	swal.showInputError("You need to write something in the input field!");
+		    	return false;
+		    }
+		    startFizzBuzz();
 		}
 	});
 }
+	
+function startFizzBuzz() {
+			inputValue = +document.querySelector('input').value;
+			for (var i = 0; i <= inputValue; i++) {
+				if (i % 3 == 0 && i % 5 == 0) {
+					x = "Fizz Buzz";
+				}
+				else if (i % 3 == 0) {
+					x = "Fizz";
+				}
+				else if (i % 5 == 0) {
+					x = "Buzz";
+				}
+				else {
+					x = i;
+				}
 
-function resetFizzBuzz() {
-	$('.resetButton').click(function() {
+				if (i == inputValue) {
+					document.getElementById("results").innerHTML = document.getElementById("results").innerHTML + x + ".";
+				}
+				else {
+					document.getElementById("results").innerHTML = document.getElementById("results").innerHTML + x + ", ";
+				}
+				swal.close();
+			}
+}
+
+function reloadFizzBuzz() {
+	$('.reloadButton').click(function() {
     	i = 0;
     	x = 0;
     	document.getElementById("results").innerHTML = "";
+    	window.location.reload();
 	});
 }
